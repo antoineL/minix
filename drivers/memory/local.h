9f7d267ee77f96e03344c24829c21e279df9141d
Antoine Leca <Antoine.Leca.1@gmail.com>
2011-08-30 12:05:06 +0200

    New way to build memory driver
    Using objcopy instead of generating a huge C source;
    this lowers significantly (minus 200 MB) the memory needed
    to remake the world using clang or gcc.

--- drivers/memory/local.h
+++ drivers/memory/local.h
@@ -2,5 +2,10 @@
 local defines and declarations 
 */
 
-extern unsigned char imgrd[];
-extern size_t imgrd_size;
+extern unsigned char _binary_image_mfs_start[], *_binary_image_mfs_end;
+extern size_t _binary_image_mfs_size;
+
+#define	imgrd		_binary_image_mfs_start
+#define	imgrd_size	_binary_image_mfs_size
+#define	imgrd_sizeX \
+	((size_t)(_binary_image_mfs_end - _binary_image_mfs_start))
