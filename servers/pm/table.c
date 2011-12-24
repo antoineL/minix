c2f5ad5392274f5a31d3d3b8ec73bfef2bfdc26e
Antoine Leca <Antoine.Leca.1@gmail.com>
2011-12-24 15:02:54 +0100

    utimens(2) system call
    Variant of utime(2) with struct timespec (with ns precision)
    instead of time_t values; also allows for tv_nsec members
    the values UTIME_NOW (force update to current time) or
    UTIME_OMIT (allow to set either atim or mtim independently.)
    
    Provides a superset of utimes(2), futimes(2), lutimes(2),
    and futimens(2).
    Provides the same subset of utimensat(2) as does NetBSD 6.

--- servers/pm/table.c
+++ servers/pm/table.c
@@ -125,6 +125,8 @@ int (*call_vec[])(void) = {
 	do_srv_kill,	/* 111 = srv_kill */
  	no_sys, 	/* 112 = gcov_flush */
 	do_get,		/* 113 = getsid	*/
+	no_sys,		/* 114 = (setpgid) */
+	no_sys,		/* 115 = (utimensat) */
 };
 /* This should not fail with "array size is negative": */
 extern int dummy[sizeof(call_vec) == NCALLS * sizeof(call_vec[0]) ? 1 : -1];
