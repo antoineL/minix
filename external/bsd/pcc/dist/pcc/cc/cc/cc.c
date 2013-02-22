a827ca6b21369e703677a15e3ef15c211fba8acf
Antoine Leca <Antoine.Leca.1@gmail.com>
2013-02-22 19:00:39 +0000

    PCC: fixes

--- external/bsd/pcc/dist/pcc/cc/cc/cc.c
+++ external/bsd/pcc/dist/pcc/cc/cc/cc.c
@@ -201,8 +201,11 @@ char *cppmdadd[] = CPPMDADD;
 #ifndef PCCLIBDIR	/* set by autoconf */
 #define PCCLIBDIR	NULL
 #endif
+#ifndef LIBDIR
+#define LIBDIR		"=/usr/lib/"	/* = for sysroot optional subst.*/
+#endif
 #ifndef DEFLIBDIRS	/* default library search paths */
-#define DEFLIBDIRS	{ "/usr/lib/", 0 }
+#define DEFLIBDIRS	{ LIBDIR, 0 }
 #endif
 #ifndef DEFLIBS		/* default libraries included */
 #define	DEFLIBS		{ "-lpcc", "-lc", "-lpcc", 0 }
@@ -847,8 +850,10 @@ main(int argc, char *argv[])
 	strlist_append(&progdirs, LIBEXECDIR);
 	if (pcclibdir)
 		strlist_append(&crtdirs, pcclibdir);
-	for (j = 0; deflibdirs[j]; j++)
+	for (j = 0; deflibdirs[j]; j++) {
 		strlist_append(&crtdirs, deflibdirs[j]);
+		strlist_append(&libdirs, deflibdirs[j]);
+	}
 
 	setup_cpp_flags();
 	setup_ccom_flags();
@@ -1138,8 +1143,10 @@ preprocess_input(char *input, char *output, int dodep)
 
 	strlist_init(&args);
 	strlist_append_list(&args, &preprocessor_flags);
-	if (ascpp)
+	if (ascpp) {
+		strlist_append(&args, "-A");
 		strlist_append(&args, "-D__ASSEMBLER__");
+	}
 	STRLIST_FOREACH(s, &includes) {
 		strlist_append(&args, "-i");
 		strlist_append(&args, s->value);
@@ -1157,6 +1164,10 @@ preprocess_input(char *input, char *output, int dodep)
 			strlist_append(&args, "-S");
 			strlist_append(&args, s->value);
 		}
+	}
+	STRLIST_FOREACH(s, &dirafterdirs) {
+		strlist_append(&args, "-S");
+		strlist_append(&args, s->value);
 	}
 	if (dodep)
 		strlist_append_list(&args, &depflags);
@@ -1618,8 +1629,8 @@ setup_cpp_flags(void)
 	strlist_append(&sysincdirs, "=" STDINC);
 #ifdef PCCINCDIR
 	if (cxxflag)
-		strlist_append(&sysincdirs, "=" PCCINCDIR "/c++");
-	strlist_append(&sysincdirs, "=" PCCINCDIR);
+		strlist_append(&sysincdirs, PCCINCDIR "/c++");
+	strlist_append(&sysincdirs, PCCINCDIR);
 #endif
 }
 
@@ -1756,6 +1767,7 @@ setup_ld_flags(void)
 {
 	char *b, *e;
 	int i;
+	struct string *s;
 
 	cksetflags(ldflgcheck, &early_linker_flags, 'a');
 	if (Bstatic == 0 && shared == 0 && rflag == 0) {
@@ -1773,9 +1785,9 @@ setup_ld_flags(void)
 		if (pcclibdir)
 			strlist_append(&late_linker_flags,
 			    cat("-L", pcclibdir));
-		for (i = 0; deflibdirs[i]; i++)
+		STRLIST_FOREACH(s, &libdirs)
 			strlist_append(&late_linker_flags,
-			    cat("-L", deflibdirs[i]));
+			    cat("-L", s->value));
 		/* standard libraries */
 		if (pgflag) {
 			for (i = 0; defproflibs[i]; i++)
